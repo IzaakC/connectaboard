964e1c6386265dcad522e8320610a8ef71b71089
Izaak Cornelis <cornelisizaak@gmail.com>
2025-01-15 09:54:19 +0100

    seen count must be exactly n

--- connectaboard/helpers.py
+++ connectaboard/helpers.py
@@ -1,14 +1,15 @@
-from dataclasses import dataclass
 from typing import Generic, TypeVar
 
 T = TypeVar("T")
 
 
-@dataclass
 class SeenCounter(Generic[T]):
-    _previous: T
+    _previous: T | None = None
     n: int
-    _seen_count = 0
+    _seen_count: int = 0
+
+    def __init__(self, n: int) -> None:
+        self.n = n
 
     def has_been_seen_n_times(self, new: T) -> bool:
         if self._previous == new:
@@ -18,4 +19,4 @@ class SeenCounter(Generic[T]):
             self._previous = new
             return False
 
-        return self._seen_count >= self.n
+        return self._seen_count == self.n
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -2,14 +2,12 @@ from connectaboard.helpers import SeenCounter
 
 
 def test_seen_counter():
-    initial = "a"
-    new = "b"
-    seen_counter = SeenCounter(initial, 3)
+    seen_counter = SeenCounter(3)
 
-    assert not seen_counter.has_been_seen_n_times(initial)
-    assert not seen_counter.has_been_seen_n_times(initial)
-    assert seen_counter.has_been_seen_n_times(initial)
+    assert not seen_counter.has_been_seen_n_times("a")
+    assert not seen_counter.has_been_seen_n_times("a")
+    assert seen_counter.has_been_seen_n_times("a")
 
-    assert not seen_counter.has_been_seen_n_times(new)
-    assert not seen_counter.has_been_seen_n_times(new)
-    assert seen_counter.has_been_seen_n_times(new)
+    assert not seen_counter.has_been_seen_n_times("b")
+    assert not seen_counter.has_been_seen_n_times("b")
+    assert seen_counter.has_been_seen_n_times("b")
